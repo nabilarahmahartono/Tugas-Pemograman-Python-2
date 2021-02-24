192356aaf13c90f1877fe873fe60f990c5402565
nabilarahmahartono <78465098+nabilarahmahartono@users.noreply.github.com>
2021-02-24 13:53:54 +0700

    Add files via upload

---
+++ test looping.py
@@ -0,0 +1,86 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 1.py
+407 bukan bilangan prima
+11 kali 37 adalah 407
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 2.py
+bilangan prima antara 900 dan 1000 adalah
+901
+903
+905
+907
+909
+911
+913
+915
+917
+919
+921
+923
+925
+927
+929
+931
+933
+935
+937
+939
+941
+943
+945
+947
+949
+951
+953
+955
+957
+959
+961
+963
+965
+967
+969
+971
+973
+975
+977
+979
+981
+983
+985
+987
+989
+991
+993
+995
+997
+999
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 3.py
+[12, 4, 3]
+[7, 5, 8]
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 4.py
+153
+370
+371
+407
+1634
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 5.py
+12 x 1 = 12
+12 x 2 = 24
+12 x 3 = 36
+12 x 4 = 48
+12 x 5 = 60
+12 x 6 = 72
+12 x 7 = 84
+12 x 8 = 96
+12 x 9 = 108
+12 x 10 = 120
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program looping 6.py
+jumlah :  3
+>>> 
